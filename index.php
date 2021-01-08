82e0c6030898e0a97341986eda8e476968f30ae2
NimbleFish <karpov2001ura@mail.ru>
2021-01-08 15:52:37 +0300

    fix netoilogy error documentation

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  session_start();
-  if (isset($_SESSION['name'])) {
-    echo 'Привет, ' . $_SESSION['name'] . '<br />';
-    echo '<a href="./exit.php" style="color:#000;">Забудь</a>';
-  } else {
-    include 'index.html';
-  }
-?>
--- web/index.php
+++ web/index.php
@@ -1,3 +1,9 @@
 <?php
-
-echo 'I\'m alive!';
+  session_start();
+  if (isset($_SESSION['name'])) {
+    echo 'Привет, ' . $_SESSION['name'] . '<br />';
+    echo '<a href="./exit.php" style="color:#000;">Забудь</a>';
+  } else {
+    include 'index.html';
+  }
+?>
